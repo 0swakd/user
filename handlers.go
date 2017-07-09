2dd23d6e38f5d9ed68151315ac14f8f328bcc44a
Antoine <antoine.bascou@gmail.com>
2017-07-10 00:09:44 +0200

    service is growing upon tns-restful-json-api

---
+++ handlers.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "net/http"
+    "strconv"
+
+    "github.com/gorilla/mux"
+)
+
+
+func UserShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    var userId int
+    var err error
+    if userId, err = strconv.Atoi(vars["userId"]); err != nil {
+        panic(err)
+    }
+    user := StorageFindUser(userId)
+    if user.Id > 0 {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(http.StatusOK)
+        if err := json.NewEncoder(w).Encode(user); err != nil {
+            panic(err)
+        }
+        return
+    }
+
+    // If we didn't find it, 404
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusNotFound)
+    if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusNotFound, Text: "Not Found"}); err != nil {
+        panic(err)
+    }
+
+}
+
+/*
+Test with this curl command:
+curl -H "Content-Type: application/json" -d '{"name":"New User"}' http://localhost:8080/todos
+*/
+func UserCreate(w http.ResponseWriter, r *http.Request) {
+    var user User
+    body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+    if err != nil {
+        panic(err)
+    }
+    if err := r.Body.Close(); err != nil {
+        panic(err)
+    }
+    if err := json.Unmarshal(body, &user); err != nil {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(422) // unprocessable entity
+        if err := json.NewEncoder(w).Encode(err); err != nil {
+            panic(err)
+        }
+    }
+
+    t := StorageCreateUser(user)
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusCreated)
+    if err := json.NewEncoder(w).Encode(t); err != nil {
+        panic(err)
+    }
+}
+
